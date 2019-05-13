d2db15496c90b6b73a9898f0afc3a6f623a867fa
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-05-13 00:15:29 -0500

    Se cambiaron querys e implementacion del carrito

--- src/db/database.js
+++ src/db/database.js
@@ -10,32 +10,38 @@ const pool = new Pool({
 
 
 // REGISTER A NEW CLIENT
-module.exports.insertNewClient = async (firstName, lastName, username, password) => {
-    
-    const values = [firstName, lastName, username, password];
-    const text = `INSERT INTO ${process.env.CLIENT_TABLE}(first_name,last_name,user_name,password) VALUES ($1,$2,$3,$4)`;
-    const res = await pool.query(text, values);
+module.exports.insertNewClient = async (firstName, lastName, email, username, password) => {
 
+    const allUser = await pool.query('SELECT * FROM client');
+    const values = [firstName, lastName, email, username, password, 1, allUser.rows.length +1];
+    console.log(allUser.rows.length + 1);
+    await pool.query(`INSERT INTO ${process.env.CART_TABLE}(name) VALUES($1)`, [username]).then(async () => {
+        console.log('Creando carrito');        
+        const text = `INSERT INTO ${process.env.CLIENT_TABLE}(first_name,last_name,email,username,password,membership,cart) VALUES ($1,$2,$3,$4,$5,$6,$7)`;
+        await pool.query(text, values).then( ()=>{
+            console.log('Valores insertados');
+        });        
+    });
 }
 
 
 // AUTHENTICATION USERNAME & PASSWORD
-module.exports.validationLogin = async(username,password)=>{
-    const res =  await pool.query(`SELECT * FROM  ${process.env.CLIENT_TABLE} WHERE user_name='${username}' AND password='${password}'`);
+module.exports.validationLogin = async (username, password) => {
+    const res = await pool.query(`SELECT * FROM  ${process.env.CLIENT_TABLE} WHERE username='${username}' AND password='${password}'`);
     return res.rows;
 }
 
 
 // SHARE ALL PRODUCTS
-module.exports.getAllProducts = async()=>{
+module.exports.getAllProducts = async () => {
     const query = `SELECT * FROM ${process.env.PRODUCT_TABLE} LIMIT 20`;
-    const res =  await pool.query(query);
+    const res = await pool.query(query);
     return res.rows;
 }
 
 // GET A PRODUCT
-module.exports.getProduct = async(id)=>{
-    const query  = `SELECT * FROM  ${process.env.PRODUCT_TABLE} WHERE id=${id}`;
+module.exports.getProduct = async (id) => {
+    const query = `SELECT * FROM  ${process.env.PRODUCT_TABLE} WHERE id=${id}`;
     const res = await pool.query(query);
     return res.rows;
 }
