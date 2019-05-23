5d67e9a3441417125633f1870fc52a623c04d2e4
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-05-22 22:49:48 -0500

    Funcionalidad de auto completado en costo total de rente en el card

--- public/js/productPage.js
+++ public/js/productPage.js
@@ -1,17 +1,23 @@
+// ANIMATIONS CARD ELEMENTS
 const firstViewOfCardProdct = document.getElementById('first-display-card');
 const waitingViewOfCardProduct = document.getElementById('waitingLoadCard');
 const lastViewOfCardProduct = document.getElementById('second-display-card');
 const loader = document.getElementById('loader');
 
-
-let startDateRent = document.getElementById("startRentData");
+// IMPORTANT ELEMENTS
+let startDateRent = document.getElementById("startRentDate");
 let endDateRent = document.getElementById("endRentData");
 let priceProduct = parseFloat(document.getElementById('price-product').textContent);
 let totalRent = document.getElementById('TotalRent');
 let cantProduct = document.getElementById("CantProduct");
 let fieldTotalRent = document.getElementById("FieldTotalRent");
-const ContinueWithPayment = () => {
 
+
+let realTotal;
+
+
+
+const ContinueWithPayment = () => {
 
     firstViewOfCardProdct.style.display = "none";
     waitingViewOfCardProduct.style.display = "block";
@@ -21,31 +27,43 @@ const ContinueWithPayment = () => {
         loader.setAttribute("class", "");
         waitingViewOfCardProduct.style.display = "none";
         lastViewOfCardProduct.style.display = "block";
-
-   }, 2300);
-} 
-
-
-
-
-function setStartRentDate (e) {
-    console.log(dayNumber(e.value));
+    }, 2300);
 }
+
+
+
+
 
 const setEndRentDate = (e) => {
-    console.log(dayNumber(e.value));
-}
+    let totalDays = (dayNumber(e.value) - dayNumber(startDateRent.value));
 
-const setQuantityProduct = (e, w) => {
-    if (e.value.length == 0) {
-
-        totalRent.textContent = priceProduct;
-        fieldTotalRent.value = priceProduct;
+    if (cantProduct.value == "0") {
+        totalRent.textContent = totalDays * priceProduct * 1;
+        fieldTotalRent.value = totalDays * priceProduct * 1;
     }
     else {
+        totalRent.textContent = totalDays * priceProduct * parseInt(cantProduct.value);
+        fieldTotalRent.value = totalDays * priceProduct * parseInt(cantProduct.value);;
+    }
 
-        totalRent.textContent = parseFloat(priceProduct) * parseInt(e.value);
-        fieldTotalRent.value = parseFloat(priceProduct) * parseInt(e.value);
+    realTotal = totalDays * priceProduct;
+
+
+}
+
+const setQuantityProduct = (e) => {
+    console.log(fieldTotalRent.value);
+    if (e.value == "0") {
+        e.value = 1;
+
+    }
+    else if (e.value.length == 0) {
+        totalRent.textContent = realTotal;
+        fieldTotalRent.value = realTotal;
+    }
+    else if (e.value.length > 0) {
+        totalRent.textContent = realTotal * e.value;
+        fieldTotalRent.value = realTotal * e.value;
     }
 
 }
