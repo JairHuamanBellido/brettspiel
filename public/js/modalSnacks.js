1d29e03ab4a25dbac898f40d34f46c3df063960a
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-05-24 20:20:46 -0500

    Funcionalidad de cambio automatico del total de precio de Snacks

--- public/js/modalSnacks.js
+++ public/js/modalSnacks.js
@@ -1,7 +1,16 @@
+let snacks = document.getElementById("TotalSnacks");
+let priceTotalSnackModal = document.getElementById("TotalSnackModal");
+let priceSnack;
 const DownQuantity =  (e)=>{
     e.disabled = false;
-    e.nextElementSibling.textContent = parseInt(e.nextElementSibling.textContent) -1;
+    priceSnack = parseInt(e.nextElementSibling.textContent);
+    e.nextElementSibling.textContent = priceSnack -1;
 
+    console.log('Total Snacks: '+parseFloat(snacks.textContent))
+    console.log('This Snacks: '+parseFloat(document.getElementById(e.parentElement.getAttribute("id")+"price").textContent) * parseInt(parseInt(e.nextElementSibling.textContent)));
+    snacks.textContent = Math.round( (parseFloat(snacks.textContent) - parseFloat(document.getElementById(e.parentElement.getAttribute("id")+"price").textContent))*100)/100;
+    priceTotalSnackModal.textContent = snacks.textContent;
+    
     if(e.nextElementSibling.textContent === "0"){
         e.disabled = true;
     }
@@ -12,7 +21,8 @@ const DownQuantity =  (e)=>{
 const UpQuantity =  (e)=>{
     
     e.previousElementSibling.textContent = parseInt(e.previousElementSibling.textContent) +1;
-    
+    snacks.textContent =Math.round( (parseFloat(snacks.textContent) +  parseFloat(document.getElementById(e.parentElement.getAttribute("id")+"price").textContent))*100)/100;
+    priceTotalSnackModal.textContent = snacks.textContent;
         if(e.previousElementSibling.textContent !== "0"){
             e.previousElementSibling.previousElementSibling.disabled =false;
         }
