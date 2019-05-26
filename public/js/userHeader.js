c37e6d63d9bffb32fd00bb3f877afd3505cfebd2
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-05-26 15:59:36 -0500

    Se añadio assets y modificaciones css de register y el header

--- public/js/userHeader.js
+++ public/js/userHeader.js
@@ -1,3 +1,31 @@
-const show  = ()=>{
-    document.getElementById("user-options").classList.toggle("show");
+let item_links = document.getElementsByClassName("item-link-options");
+let togle = false;
+let setAnimationLinkItems = (a, isClose) => {
+
+
+    let count = setInterval(() => {
+        if (a >= item_links.length) {
+            clearInterval(count);
+        }
+        if (isClose) {
+            console.log("Insertando el " + (a));
+            document.getElementById("link-item" + (a)).setAttribute("class", "vanish item-link-options");
+            a++;
+        }
+        else{
+            console.log("Sacando el " + (a));
+            document.getElementById("link-item" + (a)).setAttribute("class", "item-link-options");
+            a++;
+        }
+    }, 25);
+
+
 }
+
+
+const show = () => {
+    togle = !togle;
+    document.getElementById("user-options").classList.toggle("show");
+    setAnimationLinkItems(1,togle);
+}
+
