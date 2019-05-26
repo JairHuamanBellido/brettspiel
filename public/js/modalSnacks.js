e342b4de5cd8220c040196425a9211d1277f8f6b
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-05-25 21:08:06 -0500

    Se agrego indicador del snack para la BD

--- public/js/modalSnacks.js
+++ public/js/modalSnacks.js
@@ -6,39 +6,47 @@ let _fieldTotalRent = document.getElementById("FieldTotalRent");
 let _totalOrder = document.getElementById("TotalOrder");
 let _snacksField = document.getElementById("fieldTotalSnack");
 let _fieldTotalOrder = document.getElementById("fieldTotalOrder");
-const DownQuantity =  (e)=>{
+let snackId;
+const DownQuantity = (e) => {
+
+
+    snackId = e.parentElement.getAttribute("data-target");
+
+
     e.disabled = false;
     priceSnack = parseInt(e.nextElementSibling.textContent);
-    e.nextElementSibling.textContent = priceSnack -1;
+    e.nextElementSibling.textContent = priceSnack - 1;
 
-    
-    snacks.textContent = Math.round( (parseFloat(snacks.textContent) - parseFloat(document.getElementById(e.parentElement.getAttribute("id")+"price").textContent))*100)/100;
+
+    snacks.textContent = Math.round((parseFloat(snacks.textContent) - parseFloat(document.getElementById(e.parentElement.getAttribute("id") + "price").textContent)) * 100) / 100;
     priceTotalSnackModal.textContent = snacks.textContent;
     document.getElementById("fieldTotalSnack").value = snacks.textContent;
-    document.getElementById(e.parentElement.getAttribute("id")+"field").value = "true,"+e.nextElementSibling.textContent;
-    if(e.nextElementSibling.textContent === "0"){
-        document.getElementById(e.parentElement.getAttribute("id")+"field").value = "false,0"
+    document.getElementById(e.parentElement.getAttribute("id") + "field").value = "true," + e.nextElementSibling.textContent+","+snackId;
+    if (e.nextElementSibling.textContent === "0") {
+        document.getElementById(e.parentElement.getAttribute("id") + "field").value = "false,0"
         e.disabled = true;
     }
-  
+
     _totalOrder.textContent = Math.round((parseFloat(_fieldTotalRent.value) + parseFloat(_snacksField.value)) * 100) / 100;
     _fieldTotalOrder.value = _totalOrder.textContent;
 }
-const UpQuantity =  (e)=>{
+const UpQuantity = (e) => {
+
+    snackId = e.parentElement.getAttribute("data-target");
     
-    e.previousElementSibling.textContent = parseInt(e.previousElementSibling.textContent) +1;
-    
-    snacks.textContent =Math.round( (parseFloat(snacks.textContent) +  parseFloat(document.getElementById(e.parentElement.getAttribute("id")+"price").textContent))*100)/100;
+    e.previousElementSibling.textContent = parseInt(e.previousElementSibling.textContent) + 1;
+
+    snacks.textContent = Math.round((parseFloat(snacks.textContent) + parseFloat(document.getElementById(e.parentElement.getAttribute("id") + "price").textContent)) * 100) / 100;
     priceTotalSnackModal.textContent = snacks.textContent;
     document.getElementById("fieldTotalSnack").value = snacks.textContent;
-    if(e.previousElementSibling.textContent !== "0"){
-        
-        e.previousElementSibling.previousElementSibling.disabled =false;
+    if (e.previousElementSibling.textContent !== "0") {
+
+        e.previousElementSibling.previousElementSibling.disabled = false;
     }
-    
-    document.getElementById(e.parentElement.getAttribute("id")+"field").value = "true,"+e.previousElementSibling.textContent;
+
+    document.getElementById(e.parentElement.getAttribute("id") + "field").value = "true," + e.previousElementSibling.textContent+","+snackId;
     _totalOrder.textContent = Math.round((parseFloat(_fieldTotalRent.value) + parseFloat(_snacksField.value)) * 100) / 100;
     _fieldTotalOrder.value = _totalOrder.textContent;
-    
-    
+
+
 }
